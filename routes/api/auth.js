6b6cf26b7b42eae4040ad438428c49e7e5698599
hatim85f <50639901+hatim85f@users.noreply.github.com>
2024-12-24 01:24:41 +0400

    editing some user details

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const auth = require("../../middleware/auth");
+const isCompanyAdmin = require("../../middleware/isCompanyAdmin");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 const Organization = require("../../models/Organization");
@@ -12,6 +13,38 @@ const secretToken =
   process.env.NODE_ENV === "production"
     ? process.env.JWT_SECRET
     : config.get("jwtSecret");
+
+// @router GET api/auth
+// get user data
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.userId }).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({
+      error: "Server Error",
+      message: error.message,
+    });
+  }
+});
+
+// @router GET api/auth
+// get user data
+router.get("/all", auth, isCompanyAdmin, async (req, res) => {
+  try {
+    const user = await User.find({
+      organization: req.body.organization,
+    }).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({
+      error: "Server Error",
+      message: error.message,
+    });
+  }
+});
 
 // @router POST api/auth
 // logging user in and getting token
@@ -168,5 +201,147 @@ router.post(
     }
   }
 );
+
+// @router PUT api/auth
+// edit current user
+router.put("/:userId", auth, async (req, res) => {
+  const { userId } = req.params;
+  const { userDetails } = req.body;
+
+  try {
+    const isUser = await User.findOne({ _id: userId });
+
+    if (!isUser) {
+      return res.status(400).send({
+        error: "ERROR!",
+        message: "User Not Found",
+      });
+    }
+
+    const updatedFields = {};
+
+    if (userDetails.employmentType)
+      updatedFields.employmentType = userDetails.employmentType;
+    if (userDetails.officeLocation)
+      updatedFields.officeLocation = userDetails.officeLocation;
+    if (userDetails.subordinates)
+      updatedFields.subordinates = userDetails.subordinates;
+    if (userDetails.accessLevel)
+      updatedFields.accessLevel = userDetails.accessLevel;
+
+    if (userDetails.profile) {
+      if (userDetails.profile.firstName)
+        updatedFields["profile.firstName"] = userDetails.profile.firstName;
+      if (userDetails.profile.lastName)
+        updatedFields["profile.lastName"] = userDetails.profile.lastName;
+      if (userDetails.profile.phone)
+        updatedFields["profile.phone"] = userDetails.profile.phone;
+      if (userDetails.profile.avatar)
+        updatedFields["profile.avatar"] = userDetails.profile.avatar;
+    }
+
+    if (userDetails.settings) {
+      if (userDetails.settings.theme)
+        updatedFields["settings.theme"] = userDetails.settings.theme;
+      if (userDetails.settings.mode)
+        updatedFields["settings.mode"] = userDetails.settings.mode;
+      if (userDetails.settings.chatOn !== undefined)
+        updatedFields["settings.chatOn"] = userDetails.settings.chatOn;
+      if (userDetails.settings.notificationsOn !== undefined)
+        updatedFields["settings.notificationsOn"] =
+          userDetails.settings.notificationsOn;
+      if (userDetails.settings.newsletterOn !== undefined)
+        updatedFields["settings.newsletterOn"] =
+          userDetails.settings.newsletterOn;
+    }
+
+    if (userDetails.social) {
+      if (userDetails.social.facebook)
+        updatedFields["social.facebook"] = userDetails.social.facebook;
+      if (userDetails.social.x)
+        updatedFields["social.x"] = userDetails.social.x;
+      if (userDetails.social.linkedin)
+        updatedFields["social.linkedin"] = userDetails.social.linkedin;
+      if (userDetails.social.instagram)
+        updatedFields["social.instagram"] = userDetails.social.instagram;
+    }
+
+    // Update the user
+    const updatedUser = await User.updateOne(
+      { _id: userId },
+      { $set: updatedFields }
+    );
+
+    return res.status(200).send({
+      success: true,
+      message: "User updated successfully",
+      updatedFields,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      error: "ERROR!",
+      message: error.message,
+    });
+  }
+});
+
+// change user password by checking the old one
+router.put("/change-password/:userId", auth, async (req, res) => {
+  const { userId } = req.params;
+  const { oldPassword, newPassword } = req.body;
+
+  try {
+    const isUser = await User.findOne({ _id: userId });
+
+    if (!isUser) {
+      return res.status(400).send({
+        error: "ERROR!",
+        message: "User Not Found",
+      });
+    }
+
+    // Check if old and new passwords are the same
+
+    if (oldPassword === newPassword)
+      return res.status(400).send({
+        error: "Same password",
+        message: `New password can't be the same as old password`,
+      });
+
+    // Check if password is correct
+    const isMatch =
+      isUser.password === oldPassword ||
+      (await bcrypt.compare(oldPassword, isUser.password));
+
+    if (!isMatch) {
+      return res.status(500).send({
+        error: "Incorrect password",
+        message: `Old password is incorrect`,
+      });
+    }
+
+    // Change password
+    const salt = await bcrypt.genSalt(10);
+    hashedPassword = await bcrypt.hash(newPassword, salt);
+    await User.updateOne(
+      { _id: userId },
+      {
+        $set: {
+          password: hashedPassword,
+        },
+      }
+    );
+
+    return res.status(200).send({
+      success: true,
+      message: "Password updated successfully",
+    });
+  } catch (error) {
+    return res.status(500).send({
+      error: "ERROR!",
+      message: error.message,
+    });
+  }
+});
 
 module.exports = router;
