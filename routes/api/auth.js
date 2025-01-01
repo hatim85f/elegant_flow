f75e518cefced19da19adc0c8b7eb8db576f361d
hatim85f <50639901+hatim85f@users.noreply.github.com>
2025-01-02 02:43:19 +0400

    debugging add member

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -264,7 +264,7 @@ router.post("/invite", auth, isCompanyAdmin, async (req, res) => {
   } catch (error) {
     return res.status(500).send({
       error: "ERROR!",
-      message: "Server Error, please try again",
+      message: error.message,
     });
   }
 });
