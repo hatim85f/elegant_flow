923df4dd5a5cd152bd9a72c9b11e1981eacf84a5
hatim85f <50639901+hatim85f@users.noreply.github.com>
2025-01-03 22:01:16 +0400

    getting team members data

--- routes/api/team.js
+++ routes/api/team.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const User = require("../../models/User");
 const auth = require("../../middleware/auth");
+const Clients = require("../../models/Clients");
 
 // get managers details for the onwer to submit employees
 router.get("/managers/:organization", auth, async (req, res) => {
@@ -37,20 +38,116 @@ router.get("/:userId", auth, async (req, res) => {
   const { userId } = req.params;
 
   try {
-  } catch (error) {
     const user = await User.findOne({ _id: userId });
 
-    let team = [];
-
-    if (user.role === "owner") {
-      team = await User.find({ organization: user.organization });
-    } else if (user.role === "manager") {
-      team = await User.find({ parentId: user._id });
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found",
+        message: "The specified user does not exist.",
+      });
     }
-    return res.status(500).send({
+
+    const isOwner = user.role === "owner";
+
+    const teamPipeline = [
+      {
+        $match: isOwner
+          ? { role: "manager", organization: user.organization }
+          : { parentId: user._id },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "parentId",
+          as: "subTeam",
+        },
+      },
+      {
+        $lookup: {
+          from: "clients",
+          localField: "_id",
+          foreignField: "assignedTo",
+          as: "customers",
+        },
+      },
+      {
+        $addFields: {
+          teamMembers: { $size: "$subTeam" },
+          customersNumber: { $size: "$customers" },
+          subTeam: {
+            $map: {
+              input: "$subTeam",
+              as: "subMember",
+              in: {
+                _id: "$$subMember._id",
+                avatar: "$$subMember.avatar",
+                firstName: "$$subMember.firstName",
+                lastName: "$$subMember.lastName",
+                role: {
+                  $cond: {
+                    if: { $eq: ["$$subMember.role", "manager"] },
+                    then: "Manager",
+                    else: "Employee",
+                  },
+                },
+                officeLocation: "$$subMember.officeLocation",
+                teamMembers: {
+                  $size: {
+                    $filter: {
+                      input: "$subTeam",
+                      as: "nestedSubMember",
+                      cond: {
+                        $eq: ["$$nestedSubMember.parentId", "$$subMember._id"],
+                      },
+                    },
+                  },
+                },
+                customersNumber: {
+                  $size: {
+                    $filter: {
+                      input: "$customers",
+                      as: "customer",
+                      cond: {
+                        $eq: ["$$customer.assignedTo", "$$subMember._id"],
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          avatar: 1,
+          firstName: 1,
+          lastName: 1,
+          role: {
+            $cond: {
+              if: { $eq: ["$role", "manager"] },
+              then: "Manager",
+              else: "Employee",
+            },
+          },
+          officeLocation: 1,
+          teamMembers: 1,
+          customersNumber: 1,
+          subTeam: 1,
+        },
+      },
+    ];
+
+    const teamMembers = await User.aggregate(teamPipeline);
+
+    return res.status(200).json({ teamMembers });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
       error: "ERROR!",
-      message:
-        "An error occurred while trying to get the user's teams, please try again later",
+      message: "An error occurred while fetching team members.",
     });
   }
 });
